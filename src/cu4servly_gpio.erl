9514a1ed4630a8b745cc79b6550613bd21d7bdf6
kostochkin <kostochkin@rplab.ru>
2019-10-11 19:23:00 +0300

    gpio remove compiler warnings

--- src/cu4servly_gpio.erl
+++ src/cu4servly_gpio.erl
@@ -2,13 +2,13 @@
 -export([init/0]).
 -record(serial_port, {device = "/dev/ttyS0", speed = 500000}).
 -record(gpio_port, {path = "/sys/class/gpio", num = 24}).
--record(gpio_state, {port, latch}).
+%-record(gpio_state, {port, latch}).
 
 
 init() -> init1(#serial_port{}, #gpio_port{}, 10).
 
 
-init1(SP, GPIO, 0) -> {error, cannot_init_gpio};
+init1(_SP, _GPIO, 0) -> {error, cannot_init_gpio};
 init1(SP, GPIO, Tries) ->
 	io:format("[ GPIO ] Init ~p~n", [Tries]),
 	case filelib:is_dir(gpio_port(port_path, GPIO)) of
@@ -27,14 +27,16 @@ init1(SP, GPIO, Tries) ->
 gpio_port(num, #gpio_port{num = Num}) when is_list(Num) -> Num;
 gpio_port(Name, #gpio_port{path = Path, num = Num}) when is_list(Num) ->
 	S = case Name of
-		port_path -> "gpio" ++ Num;
-		export_path -> "export";
-		value_path -> "value";
-		dir_path -> Path
-	end;
+		port_path -> ["gpio" ++ Num];
+		export_path -> ["export"];
+		value_path -> ["value"];
+		dir_path -> []
+	end,
+	filename:join([Path | S]);
+
 
 gpio_port(Name, G = #gpio_port{num = Num}) ->
-	gpio_port(Name, G#gpio_port{num = lists:flatten(iolib:format("~p", [num]))}).
+	gpio_port(Name, G#gpio_port{num = lists:flatten(iolib:format("~p", [Num]))}).
 
 
 
